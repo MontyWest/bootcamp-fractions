e04b6e5413f25c4020ecc77546924c14ba28f0cf
Monty West <monty.p.west@gmail.com>
2018-01-24 14:48:43 +0000

    impl for reduced and double factory method

--- src/main/scala/bootcamp_fraction/MyFraction.scala
+++ src/main/scala/bootcamp_fraction/MyFraction.scala
@@ -2,19 +2,43 @@ package bootcamp_fraction
 
 class MyFraction(val numerator: Int, val denominator: Int) {
 
-  def add(other: MyFraction): MyFraction = ???
+  if (denominator == 0) throw new IllegalArgumentException
 
-  def subtract(other: MyFraction): MyFraction = ???
+  def add(other: MyFraction): MyFraction = {
+    val result0 = numerator * other.denominator
+    val result1 = denominator * other.numerator
+    val resultDenominator = denominator * other.denominator
+    val resultNumerator = result0 + result1
+    MyFraction(resultNumerator, resultDenominator)
+  }
 
-  def divide(divisor: MyFraction): MyFraction = ???
+  def subtract(other: MyFraction): MyFraction = {
+    val result0 = numerator * other.numerator
+    val result1 = denominator * other.denominator
+    val resultNumerator = result0 + result1
+    MyFraction(resultNumerator, result1)
+  }
 
-  def multiply(other: MyFraction): MyFraction = ???
+  def divide(divisor: MyFraction): MyFraction = {
+    val resultNumerator = numerator * divisor.denominator
+    val resultDenominator = divisor.numerator * denominator
+    MyFraction(resultNumerator, resultDenominator)
+  }
+
+  def multiply(other: MyFraction): MyFraction = {
+    val resultNumerator = numerator * other.numerator
+    val resultDenominator = denominator * other.denominator
+    MyFraction(resultNumerator, resultDenominator)
+  }
 
   override def toString: String = s"$numerator/$denominator"
 
   def toDouble: Double = numerator.toDouble / denominator.toDouble
 
-  def reduced: MyFraction = ???
+  def reduced: MyFraction = {
+    val gcd = MyFraction.gcd(numerator, denominator)
+    MyFraction(numerator/gcd, denominator/gcd)
+  }
 
   def canEqual(other: Any): Boolean = other.isInstanceOf[MyFraction]
 
@@ -38,7 +62,15 @@ object MyFraction {
   def apply(integer: Int): MyFraction = new MyFraction(integer, 1)
   def apply(): MyFraction = new MyFraction(1, 1)
 
-  def apply(decimal: Double): MyFraction = ???
+  def apply(decimal: Double): MyFraction = {
+    var d = decimal
+    var j = 1
+    while(!d.isValidInt) {
+      j *= 10
+      d *= j
+    }
+    MyFraction(d.toInt, j).reduced
+  }
 
   def gcd(a: Int, b: Int): Int =
     if (b == 0) a
